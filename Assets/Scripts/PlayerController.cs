2ede71052b563b9b092e6371aa3f6a054acd5b8a
janvibehal <janvibehal2@gmail.com>
2024-11-22 20:03:57 +0530

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+
+    public TextMeshProUGUI countText;
+
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+
+    private int count;
+
+    private float movementX;
+    private float movementY;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            winTextObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You WIN !!! :D";
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose :(";
+        }
+    }
+}
